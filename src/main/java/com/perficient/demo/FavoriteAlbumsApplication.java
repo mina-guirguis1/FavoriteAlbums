0afde19a03eab7b2cd97c857815ecab5a6e9f33f
mina.guirguis <mina.guirguis@perficient.com>
2022-02-03 13:11:11 -0500

    added thymeleaf successfully

--- src/main/java/com/perficient/demo/FavoriteAlbumsApplication.java
+++ src/main/java/com/perficient/demo/FavoriteAlbumsApplication.java
@@ -3,17 +3,24 @@ package com.perficient.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.perficient.demo.controller.AlbumController;
+import com.perficient.demo.service.AlbumService;
 
 @SpringBootApplication
 public class FavoriteAlbumsApplication {
-	
+	private AlbumService albumService;
 	@GetMapping("/")
 	public static void main(String[] args) {
 		SpringApplication.run(FavoriteAlbumsApplication.class, args);
 		System.out.print("test");
 	}
-
+	@GetMapping("/")
+	public String viewHomePag(Model model) {
+		model.addAttribute("listAlbums", albumService.getAllAlbums());
+		return "index";
+	}
+	
 }
--- src/main/java/com/perficient/demo/controller/AlbumController.java
+++ src/main/java/com/perficient/demo/controller/AlbumController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,14 @@ import com.perficient.demo.service.AlbumService;
 public class AlbumController {
 
 	private AlbumService albumService;
+	
+	//display list of albums 
+	@GetMapping("/")
+	public String viewHomePag(Model model) {
+		model.addAttribute("listAlbums", albumService.getAllAlbums());
+		System.out.print("got here");
+		return "index";
+	}
 
 	public AlbumController(AlbumService albumService) {
 		super();
